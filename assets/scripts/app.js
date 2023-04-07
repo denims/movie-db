867f3d7d67098fecba302d06b6dc3602d3f837a5
denims <denimallel@gmail.com>
2023-04-07 19:06:52 +0530

    Added validation for User input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@ const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop");
 const addMovieModalCancelButton = addMovieModal.querySelector(".btn.btn--passive");
+const addMovieModalAddButton = addMovieModalCancelButton.nextElementSibling;
+const userMovieInputs = addMovieModal.querySelectorAll("input");
 
 const toggleBackdrop = () => backdrop.classList.toggle("visible");
 
@@ -13,7 +15,23 @@ const toggleMovieModal = () => {
 
 const backdropClickHandler = () => toggleMovieModal();
 
+const addMovieModalAddButtonClickHandler = () => {
+    const movieTile = userMovieInputs[0].value;
+    const imageURL = userMovieInputs[1].value;
+    const rating = userMovieInputs[2].value;
+
+    if (movieTile.trim() === "" ||
+        imageURL.trim() === "" ||
+        rating.trim() === "" ||
+        rating < 0 || rating > 5) {
+        alert("Invalid user input!!");
+        return;
+    }
+};
+
 
 startAddMovieButton.addEventListener("click", toggleMovieModal)
 backdrop.addEventListener("click", backdropClickHandler)
 addMovieModalCancelButton.addEventListener("click", toggleMovieModal)
+addMovieModalAddButton.addEventListener("click", addMovieModalAddButtonClickHandler)
+
