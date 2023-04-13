5aebb61006581f9524a126a0ab3de3fed17bbe59
denims <denimallel@gmail.com>
2023-04-13 23:51:21 +0530

    Added cancel button handler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,9 @@ const backdrop = document.getElementById("backdrop");
 const addMovieModalCancelButton = addMovieModal.querySelector(".btn.btn--passive");
 const addMovieModalAddButton = addMovieModalCancelButton.nextElementSibling;
 const userMovieInputs = addMovieModal.querySelectorAll("input");
+const defaultTextBox = document.getElementById("entry-text");
+const moviesList = document.getElementById("movie-list");
+const movies = [];
 
 const toggleBackdrop = () => backdrop.classList.toggle("visible");
 
@@ -15,23 +18,78 @@ const toggleMovieModal = () => {
 
 const backdropClickHandler = () => toggleMovieModal();
 
-const addMovieModalAddButtonClickHandler = () => {
-    const movieTile = userMovieInputs[0].value;
-    const imageURL = userMovieInputs[1].value;
-    const rating = userMovieInputs[2].value;
+const cancelButtonHandler = () => {
+    toggleMovieModal();
+    clearUserMovieInput();
+};
 
-    if (movieTile.trim() === "" ||
-        imageURL.trim() === "" ||
-        rating.trim() === "" ||
-        rating < 0 || rating > 5) {
-        alert("Invalid user input!!");
-        return;
+const addMovieModalAddButtonClickHandler = () => {
+    try {
+        const movieTile = userMovieInputs[0].value;
+        const imageURL = userMovieInputs[1].value;
+        const rating = userMovieInputs[2].value;
+
+        if (movieTile.trim() === "" ||
+            imageURL.trim() === "" ||
+            rating.trim() === "" ||
+            rating < 0 || rating > 5) {
+            alert("Invalid user input!!");
+            return;
+        }
+        const newMovie = {
+            title: movieTile,
+            imageUrl: imageURL,
+            rating: rating
+        };
+        movies.push(newMovie);
+        console.log(movies);
+        updateUI();
+        renderNewMovieElement(newMovie);
+        toggleMovieModal();
+
+    } finally {
+        clearUserMovieInput();
     }
 };
 
+const clearUserMovieInput = () => {
+    for (userInput of userMovieInputs) {
+        userInput.value = "";
+    }
+};
+
+const updateUI = () => {
+    if (movies.length > 0) {
+        defaultTextBox.style.display = "none";
+    } else {
+        defaultTextBox.style.display = "block";
+    }
+};
+
+const renderNewMovieElement = (movie) => {
+    const listElement = document.createElement("li");
+    listElement.classList.add("movie-element");
+    listElement.innerHTML = `
+        <div class="movie-element__image">
+            <img src=${movie.imageUrl} alt=${movie.title}/>
+        </div>
+        <div class="movie-element__info">
+            <h2>${movie.title}</h2>
+            <p>${movie.rating}/5 stars</p>
+        </div>
+    `;
+    listElement.addEventListener("click", removeMovieElement.bind(this, listElement));
+    moviesList.append(listElement);
+}
+
+const removeMovieElement = (movieElement) => {
+    movieElement.remove();
+    movies.pop();
+    updateUI();
+}
 
 startAddMovieButton.addEventListener("click", toggleMovieModal)
 backdrop.addEventListener("click", backdropClickHandler)
-addMovieModalCancelButton.addEventListener("click", toggleMovieModal)
+addMovieModalCancelButton.addEventListener("click", cancelButtonHandler)
 addMovieModalAddButton.addEventListener("click", addMovieModalAddButtonClickHandler)
 
